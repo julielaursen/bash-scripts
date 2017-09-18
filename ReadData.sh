3e0821e047e45cc3811c0e76299a00dabed1f5af
Julie <julie.laursen@ibm.com>
2017-09-17 21:03:04 -0500

    first commit

---
+++ ReadData.sh
@@ -0,0 +1,7 @@
+# This is for taking data from user
+
+echo -n "Enter your name "
+read i
+echo "Your name is $i"
+
+
---
+++ Second.sh
@@ -0,0 +1,9 @@
+#This is a comment line
+echo "This is the basics of shell scripting"
+
+#User defined variables
+
+i=10
+j="Testing World"
+echo $i
+echo $j
